c22759e5bf52d8a705f3c3635c678e8bdb93a6e1
luiz ignacio <quineche44@gmail.com>
2023-02-24 03:40:42 -0500

    solucion del problema 1 project euler

---
+++ problema 1 project euler.cpp
@@ -0,0 +1,19 @@
+/*1) If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	float suma=0, valorRef;
+	cout<<"Ingrese el valor de referencia para consideros primos por debajo de ello : "; cin>>valorRef;
+	for(int i=1; i<valorRef; i++)
+	{
+		if(i%3==0 || i%5==0)
+		{
+			cout<<i<<endl;
+			suma=suma+i;
+		}
+	}
+	cout<<"el resultado de la suma es: "<<suma;
+	return 0;
+}
